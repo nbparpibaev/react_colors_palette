c57301c9f613878a3f2a8172a12e7066706a01d9
Nodirbek Parpibaev <nparpibaev@unist.ac.kr>
2021-02-06 00:04:22 +0900

    deploy the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Route, Switch} from "react-router-dom";
+import Palette from "./Palette";
+import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+
+  render() {
+
+    return (
+      <Switch>
+        <Route 
+          exact 
+          path="/" 
+          render={(routeProps) => 
+          <PaletteList palettes = { seedColors } {...routeProps}/>}
+        />
+        <Route 
+          exact 
+          path="/palette/:id" 
+          render={(routeProps) =>
+          <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} 
+        />
+        }/>
+        <Route 
+          exact 
+          path="/palette/:paletteId/:colorId" 
+          render={(routeProps) =>
+            <SingleColorPalette 
+            palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+            colorId = {routeProps.match.params.colorId} 
+          />}
+        />
+        <Route  render={(routeProps) => 
+          <PaletteList palettes = { seedColors } {...routeProps}/>}/>
+      </Switch>
+    );
+  }
 }
 
 export default App;
